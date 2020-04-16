0a5ac34d89c345582c873e61f43394690b5b0a6d
jcoppens <jcoppens@ghgsat.com>
2020-04-16 12:36:39 -0400

    Initial commit

---
+++ reddit-scraper-inator.py
@@ -0,0 +1,9 @@
+# Filename: reddit-scraper-inator.py  SRC: J.Coppens 2020
+
+import praw
+import pandas as pd
+
+SUBREDDIT = 'PrequelMemes'
+
+reddit = praw.Reddit(user-agent='Scraper test script')
+subreddit = reddit.subreddit('pre
