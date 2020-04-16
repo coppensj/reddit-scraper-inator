bcdeb750128b2a527cab84accd1561e979540f2b
jcoppens <jcoppens@ghgsat.com>
2020-04-16 16:07:24 -0400

    Fully implements user post scraper, data saved to csv and images saved in 'Images/'

--- reddit-scraper-inator.py
+++ reddit-scraper-inator.py
@@ -3,8 +3,10 @@
 import praw
 import pandas as pd
 import config
+from PIL import Image
+import urllib, io
+import datetime as dt
 
-SUBREDDIT = 'PrequelMemes'
 
 reddit = praw.Reddit(client_id = config.PERSONAL_USE_SCRIPT, \
                      client_secret = config.SECRET_KEY, \
@@ -12,8 +14,60 @@ reddit = praw.Reddit(client_id = config.PERSONAL_USE_SCRIPT, \
                      username = 'rocket-sheep', \
                      password = config.PASSWORD)
 
-subreddit = reddit.subreddit(SUBREDDIT)
+thibbyboy = reddit.redditor('Thibson34')
 
-for submission in subreddit.top(limit=50):
-    print(submission.title, submission.score)
+title_string = 'Every day General Grievous adds a unique lightsaber to his collection.'
 
+posts = {
+        'date': [],
+        'day': [],
+        'description': [],
+        'karma': [],
+        # 'karma_ratio': [],
+        'num_comments': [],
+        'silv': [],
+        'gold': [],
+        'plat': [],
+        'img_file': []}
+
+for submission in thibbyboy.submissions.new(limit=200):
+    if submission.subreddit not in ['PrequelMemes', 'HistoryMemes']:
+        continue    
+    if title_string not in submission.title:
+        continue
+    else:
+        print(submission.title)
+        title = submission.title.split('.')
+        day = int(title[1].split(':')[0][5:7])
+        posts['date'].append(dt.date.fromtimestamp(submission.created_utc))
+        posts['day'].append(day)
+        posts['description'].append(title[1].split(':')[1][1:])
+        posts['karma'].append(submission.score)
+        # posts['karma_ratio'].append(submission.upvote_ratio)
+        posts['num_comments'].append(submission.num_comments)
+
+        if 'gid_1' in submission.gildings.keys():
+            posts['silv'].append(submission.gildings['gid_1'])
+        else:
+            posts['silv'].append(0)
+        if 'gid_2' in submission.gildings.keys():
+            posts['gold'].append(submission.gildings['gid_2'])
+        else:
+            posts['gold'].append(0)
+        if 'gid_3' in submission.gildings.keys():
+            posts['plat'].append(submission.gildings['gid_3'])
+        else:
+            posts['plat'].append(0)
+        
+        filename = f"Images/day_{day}.png"
+        if day in [53, 65]:
+            with urllib.request.urlopen(submission.url) as url:
+                f = io.BytesIO(url.read())
+            im = Image.open(f)
+            im = im.convert('RGB')
+            im.save(filename)
+        posts['img_file'].append(filename)
+
+posts = pd.DataFrame(posts).sort_values(by=['day'])
+print(posts)
+posts.to_csv(r'thibson34_grievous_post_data.csv')
