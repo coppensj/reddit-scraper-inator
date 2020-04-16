7b8eb1822bebeb7fb21eb37613b510fbb587d9ba
jcoppens <jcoppens@ghgsat.com>
2020-04-16 13:10:10 -0400

    Adds proper credentials to initialize. Quick scraping test of r/PrequelMemes

--- reddit-scraper-inator.py
+++ reddit-scraper-inator.py
@@ -2,8 +2,18 @@
 
 import praw
 import pandas as pd
+import config
 
 SUBREDDIT = 'PrequelMemes'
 
-reddit = praw.Reddit(user-agent='Scraper test script')
-subreddit = reddit.subreddit('pre
+reddit = praw.Reddit(client_id = config.PERSONAL_USE_SCRIPT, \
+                     client_secret = config.SECRET_KEY, \
+                     user_agent = 'reddit-scraper-inator', \
+                     username = 'rocket-sheep', \
+                     password = config.PASSWORD)
+
+subreddit = reddit.subreddit(SUBREDDIT)
+
+for submission in subreddit.top(limit=50):
+    print(submission.title, submission.score)
+
